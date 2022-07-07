7c37f4d716f6044e3d0b9ca1111f4852bb2c8b00
marvelman3284 <jedimaster3284@gmail.com>
2022-07-07 15:26:03 -0400

    clean up code, check for valid color guesses

--- client.py
+++ client.py
@@ -18,9 +18,18 @@ def send_guess() -> list[str]:
     print(f"It is your turn to try to guess the code. Here are the possibilities:\n{poss}")
     for _ in range(4):
         code = input("What is your guess?(order matters): ")
-        guess.append(code)
+
+        while not check_guess(poss, code):
+            print("That's not a valid choice! Try again:")
+            code = input("What is your guess?(order matters): ")
+        else:
+            guess.append(code)
 
     return guess
+
+
+def check_guess(colors: list[str], guess: str) -> bool:
+    return guess in colors
 
 
 def format_color_codes(color_codes: list[str]):
@@ -29,24 +38,26 @@ def format_color_codes(color_codes: list[str]):
     return f"There is/are {color['r']} color(s) that are in the right place. There is/are {color['c']} color(s) that are in the wrong spot. There is/are {color['n']} color(s) that aren't used in the code"
 
 
+def main():
+    HOST = "127.0.0.1"  # The server's hostname or IP address
+    PORT = 65432  # The port used by the server
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        code = pickle.loads(s.recv(4096))
+
+        for _ in range(10):
+            guess = send_guess()
+            s.sendall(pickle.dumps(guess))
+            color = pickle.loads(s.recv(4096))
+            print(format_color_codes(color))
+            if guess == code: 
+                print("You win! You guessed the code!")
+                quit()
+
+        print("You lose! You failed to guess the code in 20 turns!")
+        quit()
 
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    code = pickle.loads(s.recv(4096))
-
-    for _ in range(20):
-        guess = send_guess()
-        s.sendall(pickle.dumps(guess))
-        color = pickle.loads(s.recv(4096))
-        print(format_color_codes(color))
-        if guess == code: 
-            print("You win! You guessed the code!")
-            break
-
-    print("You lose! You failed to guess the code in 20 turns!")
-    quit()
-
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -36,18 +36,18 @@ def color_code(code: list[str], guess: list[str]) -> list[str]:
     color: list[str] = []
     print(f"For a reminder, here is your set code: {code}")
     for i in guess:
-        print(f"Is {i} in the right place but the wrong color (c), the right place and right color (r) or wrong color (n): ")
+        print(f"Is {i} the right color but the wrong place (c), the right place and right color (r) or wrong color (n): ")
         c = input(">>> ")
         color.append(c)
 
     return color
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
 
 def main():
-    win = False
+    HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+    PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
@@ -55,23 +55,20 @@ def main():
 
         with conn:
             print(f"Connected by {addr}")
-            while not win:
-                code = create_code()
-                if code: 
-                    conn.sendall(pickle.dumps(code))
+            code = create_code()
+            conn.sendall(pickle.dumps(code))
 
-                    for _ in range(20):
-                        try:
-                            guess = pickle.loads(conn.recv(4096))
-                            color = color_code(code, guess)
-                            conn.sendall(pickle.dumps(color))
-                        except EOFError:
-                            print("You lose! The codebreakers guessed the code!")
-                            win = True
-                            break
-
-                    print("You win! The codebreaker didn't guess the code in 20 turns!")
+            for _ in range(10):
+                try:
+                    guess = pickle.loads(conn.recv(4096))
+                    color = color_code(code, guess)
+                    conn.sendall(pickle.dumps(color))
+                except EOFError:
+                    print("You lose! The codebreakers guessed the code!")
                     quit()
+
+            print("You win! The codebreaker didn't guess the code in 10 turns!")
+            quit()
 
 
 if __name__ == "__main__":
